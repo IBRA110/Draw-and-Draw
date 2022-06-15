5da3692698d36169f81a0d4641b0eb74d7f3cb34
Ibrahim <mamedovarbeiter@gmail.com>
2022-06-15 19:42:33 +0300

    version 0.01

---
+++ README.md
@@ -0,0 +1,13 @@
+# Drawing site
+
+
+# to install packages run 
+. deploy
+
+# to start backend server run 
+. run-backend
+
+# to start frontend server run 
+. run-frontend
+
+
---
+++ backend/project/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CreateUserView, LoginView
+
+urlpatterns = [
+    path('registration/', CreateUserView.as_view(),name='signup'),
+    path('login/', LoginView.as_view(),name='login')
+
+]
