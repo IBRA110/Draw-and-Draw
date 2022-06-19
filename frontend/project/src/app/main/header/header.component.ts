2499b772f999e29977b53e142fadd6bf27620766
Ibrahim <mamedovarbeiter@gmail.com>
2022-06-20 00:46:21 +0300

    version 0.07

--- frontend/project/src/app/main/header/header.component.ts
+++ frontend/project/src/app/main/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Subscription } from 'rxjs';
+import { Injectable } from '@angular/core';
+
 import { AuthService } from '../../auth/auth.service';
 
 @Component({
@@ -7,14 +10,21 @@ import { AuthService } from '../../auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+@Injectable({
+    providedIn: 'root'
+})
 export class HeaderComponent implements OnInit, OnDestroy {
 	
 	isAuthenticated: boolean = false
   private userSub: Subscription
+	public authComp: any
 
 	constructor(
-		private auth: AuthService
-	) { }
+		private auth: AuthService,
+		@Inject(DOCUMENT) private document: Document
+	) { 
+		this.authComp = this.document.getElementsByClassName('forms')[0]
+	}
 
   ngOnInit(): void {
 		this.userSub = this.auth.user.subscribe((user) => {
@@ -23,6 +33,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 	ngOnDestroy(){
 		this.userSub.unsubscribe()
+	}
+	popupShow(){
+		this.authComp.classList.add('show')
+	}
+	popupHide(){
+		this.authComp.classList.remove('show')
 	}
 	logout(){
 		this.auth.logout()
--- frontend/project/src/app/main/home/home.component.ts
+++ frontend/project/src/app/main/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderComponent } from '../header/header.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	
 
-  constructor() { }
+	constructor(
+		public head: HeaderComponent
+	) {
+
+	}
 
   ngOnInit(): void {
   }
-
+	
+	popupShow(){
+		this.head.popupShow()
+	}
+	popupHide(){
+		this.head.popupHide()
+	}
 }
